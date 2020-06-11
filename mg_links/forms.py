e642852f8ea97b66d489dbdb1bca316efc551a67
Justin Montgomery <montgomerycodepro@gmail.com>
2020-06-11 15:26:36 -0400

    fixed spelling errors

--- mg_links/forms.py
+++ mg_links/forms.py
@@ -3,7 +3,7 @@ from .models import Product
 
 class AddProduct(forms.Form):
     product_name = forms.CharField(max_length=200, label='Product')
-    key_word = forms.CharField(max_length=200, label='Key_word ', widget=forms.TextInput(attrs={'placeholder': 'Future search term'}))
+    key_word = forms.CharField(max_length=200, label='Keyword ', widget=forms.TextInput(attrs={'placeholder': 'Future search term'}))
     link = forms.CharField(max_length=500, label='Link') 
 
     class Meta:
@@ -11,5 +11,5 @@ class AddProduct(forms.Form):
 
 
 class SearchProduct(forms.Form):
-    looking_for = forms.CharField(max_length=200, label='Key_word search')
+    looking_for = forms.CharField(max_length=200, label='Keyword search')
 
